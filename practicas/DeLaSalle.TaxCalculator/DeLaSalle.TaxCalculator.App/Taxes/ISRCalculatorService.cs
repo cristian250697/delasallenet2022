7b3f7194679ac91c3c52ed0ce7616c726f4d9781
Angel Christian Alvarez Trujillo <cristian_alvarez2506@outlook.com>
2022-10-31 20:22:51 -0600

    Tax calculator fixed

--- practicas/DeLaSalle.TaxCalculator/DeLaSalle.TaxCalculator.App/Taxes/ISRCalculatorService.cs
+++ practicas/DeLaSalle.TaxCalculator/DeLaSalle.TaxCalculator.App/Taxes/ISRCalculatorService.cs
@@ -10,19 +10,19 @@ public class ISRCalculatorService
     public ISRCalculatorService()
     {
         // Annual ISR Table 2022 
-        _limits = new List<Limit
+        _limits = new List<Limit>
         {
             new Limit{ LowerLimit = 0.01, UpperLimit = 7735.00 , FixedFee = 0.00,  SurplusPercentage = 1.92},
-            new Limit{ LowerLimit = 735.01, UpperLimit = 65651.07 , FixedFee = 148.51,  SurplusPercentage = 6.40},
-            new Limit{ LowerLimit = 65651.08, UpperLimit = 115375.90 , FixedFee = 3855.14,  SurplusPercentage = 1088},
-            new Limit{ LowerLimit = 15375.91, UpperLimit = 134119.41 , FixedFee = 9265.20,  SurplusPercentage = 16.00},
+            new Limit{ LowerLimit = 7735.01, UpperLimit = 65651.07 , FixedFee = 148.51,  SurplusPercentage = 6.40},
+            new Limit{ LowerLimit = 65651.08, UpperLimit = 115375.90 , FixedFee = 3855.14,  SurplusPercentage = 10.88},
+            new Limit{ LowerLimit = 115375.91, UpperLimit = 134119.41 , FixedFee = 9265.20,  SurplusPercentage = 16.00},
             new Limit{ LowerLimit = 134119.42, UpperLimit = 160577.65 , FixedFee = 12264.16,  SurplusPercentage = 17.92},
-            new Limit{ LowerLimit = 160577.661, UpperLimit = 23862.00 , FixedFee = 17005.47,  SurplusPercentage = 21.36},
+            new Limit{ LowerLimit = 160577.66, UpperLimit = 323862.00 , FixedFee = 17005.47,  SurplusPercentage = 21.36},
             new Limit{ LowerLimit = 323862.01, UpperLimit = 510451.00 , FixedFee = 51883.01,  SurplusPercentage = 23.52},
             new Limit{ LowerLimit = 510451.01, UpperLimit = 974535.03 , FixedFee = 95768.74,  SurplusPercentage = 30.00},
             new Limit{ LowerLimit = 974535.04, UpperLimit = 1299380.04 , FixedFee = 234993.95,  SurplusPercentage = 32.00},
             new Limit{ LowerLimit = 1299380.05, UpperLimit = 3898140.12, FixedFee = 338944.34,  SurplusPercentage = 34.00},
-            new Limit{ LowerLimit  3898140.13, UpperLimit = 1.7976931348623157E+308, FixedFee = 1222522.76,  SurplusPercentage = 35.00},
+            new Limit{ LowerLimit = 3898140.13, UpperLimit = 1.7976931348623157E+308, FixedFee = 1222522.76,  SurplusPercentage = 35.00},
         };
     }
     
@@ -52,10 +52,10 @@ public class ISRCalculatorService
         // isr = result (368.95936) + (limit.FixedFee) 148.51
         
         var baseAmount = salary.Amount - limit.LowerLimit;
-        var result = baseAmount * (limit.SurplusPercentage / 100)
+        var result = baseAmount * (limit.SurplusPercentage / 100);
         var isr = result + limit.FixedFee;
         
-        return isr;
+        return Math.Round(isr, 6, MidpointRounding.AwayFromZero);
     }
 
 }
---
+++ practicas/DeLaSalle.TaxCalculator/DeLaSalle.TaxCalculator.Tests/IsrShould.cs
@@ -0,0 +1,172 @@
+﻿using DeLaSalle.TaxCalculator.App.Taxes;
+using DeLaSalle.TaxCalculator.Core.Entities;
+
+namespace DeLaSalle.TaxCalculator.Tests;
+
+public class IsrShould
+{
+    [Fact]
+    public void Isr_WhenSetSalaryAmount7000_Returns_134_399808()
+    {
+        // Arrange
+        double expectedType = 134.399808;
+        var salary = new Salary{Amount = 7000};
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount36500_Returns_1989_46936()
+    {
+        // Arrange
+        double expectedType = 1989.46936;
+        var salary = new Salary{Amount = 36500};
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount110000_Returns_8680_302496()
+    {
+        // Arrange
+        double expectedType = 8680.302496;
+        var salary = new Salary{Amount = 110000};
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount128300_Returns_11333_0544()
+    {
+        // Arrange
+        double expectedType = 11333.0544;
+        var salary = new Salary{Amount = 128300};
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount156800_Returns_16328_519936()
+    {
+        // Arrange
+        double expectedType = 16328.519936;
+        var salary = new Salary{Amount = 156800};
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount223840_Returns_30518_305824()
+    {
+        // Arrange
+        double expectedType = 30518.305824;
+        var salary = new Salary{Amount = 223840 };
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount431000_Returns_77081_865248()
+    {
+        // Arrange
+        double expectedType = 77081.865248;
+        var salary = new Salary{Amount = 431000 };
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount866700_Returns_202643_437()
+    {
+        // Arrange
+        double expectedType = 202643.437;
+        var salary = new Salary{Amount = 866700 };
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount1120500_Returns_281702_7372()
+    {
+        // Arrange
+        double expectedType = 281702.7372;
+        var salary = new Salary{Amount = 1120500 };
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount2399523_Returns_712992_943()
+    {
+        // Arrange
+        double expectedType = 712992.943;
+        var salary = new Salary{Amount = 2399523 };
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+    
+    [Fact]
+    public void Isr_WhenSetSalaryAmount7350966_Returns_2431011_8145()
+    {
+        // Arrange
+        double expectedType = 2431011.8145;
+        var salary = new Salary{Amount = 7350966 };
+        var sut = new ISRCalculatorService();
+
+        // Act
+        var result = sut.GetISR(salary);
+        
+        // Asert
+        Assert.Equal(expectedType, result);
+    }
+}
